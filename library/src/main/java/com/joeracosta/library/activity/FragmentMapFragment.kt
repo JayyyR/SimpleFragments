56fa90630f535381984e01e1c573237b12d2cfa1
unknown <flatpita92@gmail.com>
2017-12-23 20:39:06 -0500

    mapfragment to kotlin

--- library/src/main/java/com/joeracosta/library/activity/FragmentMapFragment.kt
+++ library/src/main/java/com/joeracosta/library/activity/FragmentMapFragment.kt
@@ -1,16 +1,15 @@
 package com.joeracosta.library.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by Joe on 8/14/2017.
- * Meant to be a shell map activity that has a map of SimpleFragments. Back presses etc are handled for you.
+ * Meant to be a shell map fragment that has a map of SimpleFragments. Back presses etc are handled for you.
  * The back presses will be sent to the SimpleFragment thats currently shown in the map and handled there. If it's not handled
  * It will be sent to this activity. You shouldn't inflate a layout here inside the fragment container that is meant to be visible
  * to the user.
  */
-abstract class FragmentMapActivity : AppCompatActivity() {
+abstract class FragmentMapFragment : SimpleFragment() {
 
     protected var mCurrentFragment: SimpleFragment? = null
         private set
@@ -21,7 +20,7 @@ abstract class FragmentMapActivity : AppCompatActivity() {
 
         savedInstanceState?.getString(CURRENT_FRAG_TAG)?.let {
             mCurrentFragmentTag = it
-            mCurrentFragment = supportFragmentManager.findFragmentByTag(mCurrentFragmentTag) as SimpleFragment
+            mCurrentFragment = childFragmentManager.findFragmentByTag(mCurrentFragmentTag) as SimpleFragment
         }
     }
 
@@ -31,7 +30,7 @@ abstract class FragmentMapActivity : AppCompatActivity() {
     }
 
     fun hasFragments(): Boolean {
-        return supportFragmentManager.fragments.size > 0
+        return childFragmentManager.fragments.size > 0
     }
 
     /**
@@ -45,11 +44,11 @@ abstract class FragmentMapActivity : AppCompatActivity() {
                           tag: String) {
         var fragmentToActuallyAdd = fragmentToAdd
 
-        supportFragmentManager.findFragmentByTag(tag)?.let{
+        childFragmentManager.findFragmentByTag(tag)?.let {
             fragmentToActuallyAdd = it as SimpleFragment
         }
 
-        val fragmentTransaction = supportFragmentManager.beginTransaction().apply {
+        val fragmentTransaction = childFragmentManager.beginTransaction().apply {
             if (mCurrentFragment != null) {
                 hide(mCurrentFragment)
             }
@@ -67,20 +66,23 @@ abstract class FragmentMapActivity : AppCompatActivity() {
         fragmentTransaction.commitAllowingStateLoss()
     }
 
-    override fun onBackPressed() {
-        if (!handleBackPress()) {
-            super.onBackPressed()
-        }
-    }
-
-    /**
-     * Passes the back press to children fragments so they can consume it if they'd like
-     * @return whether or not the back press was consumed by a child fragment
-     */
-    protected fun handleBackPress() : Boolean{
+    override fun onSimpleBackPressed(): Boolean {
         return mCurrentFragment?.onSimpleBackPressed() ?: false
     }
 
+    override fun onHiddenChanged(hidden: Boolean) {
+        super.onHiddenChanged(hidden)
+
+        mCurrentFragment?.let {
+            if (hidden) {
+                it.setAtForefront(false)
+                it.onHidden()
+            } else {
+                it.setAtForefront(true)
+                it.onShown()
+            }
+        }
+    }
 }
 
-private const val CURRENT_FRAG_TAG = "com.joeracosta.current_frag_tag_activity_map"
+private const val CURRENT_FRAG_TAG = "com.joeracosta.current_frag_tag_fragment_map"
